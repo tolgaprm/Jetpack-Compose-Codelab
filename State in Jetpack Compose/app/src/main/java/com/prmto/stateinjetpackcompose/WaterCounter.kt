dabaae93f2c0bd1ceb2c71a9a2d27eec1cab36a1
Tolga Pirim <tolgapirim25@gmail.com>
2022-08-19 16:49:17 +0300

    Memory in a composable function

---
+++ State in Jetpack Compose/app/src/main/java/com/prmto/stateinjetpackcompose/WaterCounter.kt
@@ -0,0 +1,22 @@
+package com.prmto.stateinjetpackcompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        // Changes to count are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
+
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+}
